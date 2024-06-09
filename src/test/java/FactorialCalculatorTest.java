066208c18790255a406d34cec9655ea7a0773c4b
Shosho243 <dangson243@gmail.com>
2024-06-09 18:37:35 +0300

    Lesson_12_junit_5

--- src/test/java/FactorialCalculatorTest.java
+++ src/test/java/FactorialCalculatorTest.java
@@ -1,4 +1,5 @@
 import Factorial.FactorialCalculator;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -6,19 +7,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FactorialCalculatorTest {
+    private FactorialCalculator calcilator;
+    @BeforeEach
+    void setUp(){
+        calcilator = new FactorialCalculator();
+    }
     @DisplayName("Позитивный тест при вводе положительного числа ")
     @Test
     void testCalculateFactorialWithPositiveNumber() {
-        assertEquals(120, FactorialCalculator.calculateFactorial(5));
-        assertEquals(1, FactorialCalculator.calculateFactorial(0));
-        assertEquals(1, FactorialCalculator.calculateFactorial(1));
+        assertEquals(120, calcilator.calculateFactorial(5));
+        assertEquals(1, calcilator.calculateFactorial(0));
+        assertEquals(1, calcilator.calculateFactorial(1));
     }
 
     @DisplayName("Негативный тест при вводе отрицательного числа  ")
     @Test
     void testCalculateFactorialWithNegativeNumber() {
         assertThrows(IllegalArgumentException.class, () -> {
-            FactorialCalculator.calculateFactorial(-1);
+            calcilator.calculateFactorial(-1);
         });
     }
 }
