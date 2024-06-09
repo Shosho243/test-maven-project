5f79e691f42f1b5fbce1f1740876b0af3d2a5456
Shosho243 <dangson243@gmail.com>
2024-06-09 15:56:25 +0300

    Lesson13

--- src/main/java/Homework13/ArrayOperation.java
+++ src/main/java/Homework13/ArrayOperation.java
@@ -6,18 +6,27 @@ public class ArrayOperation {
     public static void main(String[] args) {
         String[] wordsArray = {"apple", "banana", "apple", "orange", "banana",
                 "grape", "kiwi", "apple", "orange", "apple"};
-        List<String> wordsList = Arrays.asList(wordsArray);
-        Set<String> uniqueWords = new HashSet<>(wordsList);
-        System.out.println("Уникальные слова");
-        for (String word : uniqueWords) {
-            System.out.println(word);
+        List<String> unique = new ArrayList<>();
+        Map<String, Integer> repeatCount = new HashMap<>();
+        for (int i = 0; i < wordsArray.length; i++) {
+            int count = 0;
+            for (int j = 0; j < wordsArray.length; j++) {
+                if (wordsArray[i].equals(wordsArray[j]) && i != j) {
+                    count++;
+                }
+            }
+            repeatCount.put(wordsArray[i], count + 1);
+            if (!unique.contains(wordsArray[i]) && count == 0) {
+                unique.add(wordsArray[i]);
+            }
         }
-        Map<String, Integer> wordCount = new HashMap<>();
-        for (String word : wordsList) {
-            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+        System.out.println("Список уникальных слов:");
+        for (String word : unique) {
+            System.out.print(word + " ");
         }
-        System.out.println("\nКоличевство повторений каждого слова : ");
-        for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
+        System.out.println();
+        System.out.println("\nКоличество вхождений каждого слова:");
+        for (Map.Entry<String, Integer> entry : repeatCount.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
     }
